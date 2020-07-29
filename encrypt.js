2f4352175ea91e36b36cfadeac7fe9090aeebdec
Pierluigi Cau <pierluigi@github.com>
2020-07-29 14:29:56 +0100

    Initial

---
+++ encrypt.js
@@ -0,0 +1,25 @@
+const sodium = require("tweetsodium");
+const octokit = require("@actions/github");
+const dotenv = require("dotenv"); // TODO remove for Action environment
+
+dotenv.config();
+
+let encrypt = async function (secretValue) {
+  const github = octokit.getOctokit(process.env.ACTION_PAT);
+
+  const {
+    data: { key: publicKey },
+  } = await github.actions.getOrgPublicKey({
+    org: process.env.GITHUB_ORG,
+  });
+
+  const encrypted = sodium.seal(
+    Buffer.from(secretValue),
+    Buffer.from(publicKey, "base64")
+  );
+  console.log(Buffer.from(encrypted).toString("base64"));
+
+  return encrypted;
+};
+
+module.exports = encrypt;
--- index.test.js
+++ index.test.js
@@ -1,23 +1,29 @@
-const wait = require('./wait');
-const process = require('process');
-const cp = require('child_process');
-const path = require('path');
+// const wait = require("./wait");
+const process = require("process");
+const cp = require("child_process");
+const path = require("path");
+const encrypt = require("./encrypt");
 
-test('throws invalid number', async () => {
-  await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
-});
+// test('throws invalid number', async () => {
+//   await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
+// });
 
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThan(450);
+// test('wait 500 ms', async () => {
+//   const start = new Date();
+//   await wait(500);
+//   const end = new Date();
+//   var delta = Math.abs(end - start);
+//   expect(delta).toBeGreaterThan(450);
+// });
+
+test("encrypt a value", async () => {
+  let res = await encrypt("key", "value");
+  console.log(res);
 });
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 500;
-  const ip = path.join(__dirname, 'index.js');
-  console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
-})
+// test("test runs", () => {
+//   process.env["INPUT_MILLISECONDS"] = 500;
+//   const ip = path.join(__dirname, "index.js");
+//   console.log(cp.execSync(`node ${ip}`, { env: process.env }).toString());
+// });
