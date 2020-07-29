cd5c39d2ca1bf33b4a6e59e1b4ffc15dd6ba50fd
Pierluigi Cau <pierluigi@github.com>
2020-07-29 17:33:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const encrypt = require("./encrypt");
 async function run() {
   try {
     const value = core.getInput("value");
+    console.log("Sorry, just debuggin'");
+    console.log(value);
     console.log(`Owner: ${process.env.INPUT_OWNER`);
-    const encrypted = await encrypt(value);
-    core.info(`Encrypted ${encrypted} from ${value}`);
-    console.log(encrypted);
+//     const encrypted = await encrypt(value);
+//     core.info(`Encrypted ${encrypted} from ${value}`);
+//     console.log(encrypted);
     // TODO store secret
   } catch (error) {
     core.setFailed(error.message);
