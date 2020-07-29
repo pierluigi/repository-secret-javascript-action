ef65b1c50f3115bfbed34a8fe55dfdf945ed535b
Pierluigi Cau <pierluigi@github.com>
2020-07-29 14:36:06 +0100

    Test broken, but it works

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const encrypt = require("./encrypt");
 
 async function run() {
   try {
-    const encrypted = await encrypt(value);
+    const encrypted = await encrypt("test string");
+    console.log(encrypted);
   } catch (error) {
     core.setFailed(error.message);
   }
--- index.test.js
+++ index.test.js
@@ -1,29 +1,12 @@
 // const wait = require("./wait");
-const process = require("process");
-const cp = require("child_process");
-const path = require("path");
 const encrypt = require("./encrypt");
 
-// test('throws invalid number', async () => {
-//   await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
-// });
-
-// test('wait 500 ms', async () => {
-//   const start = new Date();
-//   await wait(500);
-//   const end = new Date();
-//   var delta = Math.abs(end - start);
-//   expect(delta).toBeGreaterThan(450);
-// });
-
 test("encrypt a value", async () => {
-  let res = await encrypt("key", "value");
-  console.log(res);
+  try {
+    const encrypted = await encrypt("test string");
+    console.log(encrypted);
+  } catch (error) {
+    //     TypeError: unexpected type, use Uint8Array WTF
+    console.error(error);
+  }
 });
-
-// shows how the runner will run a javascript action with env / stdout protocol
-// test("test runs", () => {
-//   process.env["INPUT_MILLISECONDS"] = 500;
-//   const ip = path.join(__dirname, "index.js");
-//   console.log(cp.execSync(`node ${ip}`, { env: process.env }).toString());
-// });
